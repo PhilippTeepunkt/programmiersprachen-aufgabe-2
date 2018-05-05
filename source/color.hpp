aa10ef0caebb777cddb62afc6e131aa68e49a625
Philipp Tornow <philipp_oeffentlich@hotmail.de>
2018-05-05 03:37:30 +0200

    style changes

--- source/color.hpp
+++ source/color.hpp
@@ -9,7 +9,7 @@ struct Color{
     float _g;
     float _b;
 
-    Color{float value}{
+    Color(float value){
         if(value<0&&value>1){
             std::cout<<"The value is not a valid inut! black was constructed instead! ";
             _r = 0.0f;
@@ -25,9 +25,9 @@ struct Color{
         }
     };
 
-    Color{float r,float g,float b}{
+    Color(float r,float g,float b){
         
-        if(r,g,b>1&&r,g,b<0){
+        if(r>1&&r<0&&g>1&&g<0&&b<0&&b>1){
 
             std::cout<<"The values are not a valid inut! black was constructed instead! ";
             _r = 0.0f;
@@ -44,5 +44,5 @@ struct Color{
     }
 
     float r,g,b;
-}
+};
 #endif
--- source/tests.cpp
+++ source/tests.cpp
@@ -2,6 +2,9 @@
 #include <catch.hpp>
 #include "vec2.hpp"
 #include "mat2.hpp"
+#include "color.hpp"
+#include "rectangle.hpp"
+#include "circle.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -9,243 +12,245 @@ int main(int argc, char *argv[])
 }
 
 //aufgabe 2.2 und 2.3
-
-TEST_CASE ("default_vec2","[vec2]")
+TEST_CASE ("tests_vec2","[vec2]")
 {
-  Vec2 vec{3.0f,2.0f};
+  SECTION ("vec2_constructors")
+  {
+    Vec2 vec{3.0f,2.0f};
 
-  REQUIRE(vec.x == 3.0f);
-  REQUIRE(vec.y == 2.0f);
+    REQUIRE(vec.x == 3.0f);
+    REQUIRE(vec.y == 2.0f);
+
+
+    Vec2 vecCustom;
+    REQUIRE(vecCustom.x == 0.0f);
+    REQUIRE(vecCustom.y == 0.0f);
+  }
 }
 
-TEST_CASE ("test_vec2","[vec2]")
+TEST_CASE ("test_vec2_operator","[vec2]")
 {
-  Vec2 vecCustom;
-  REQUIRE(vecCustom.x == 0.0f);
-  REQUIRE(vecCustom.y == 0.0f);
-}
+  SECTION("test_+=operator")
+  {
+    Vec2 vec{3.0f,2.0f};
+    Vec2 vecT{5.0f,3.0f};
+    vec+=vecT;
+    REQUIRE(vec.x==8.0f);
+    REQUIRE(vec.y==5.0f);
+  }
 
-TEST_CASE ("test_vec2_+=operator","[vec2_operator]")
-{
-  Vec2 vec{3.0f,2.0f};
-  Vec2 vecT{5.0f,3.0f};
-  vec+=vecT;
-  REQUIRE(vec.x==8.0f);
-  REQUIRE(vec.y==5.0f);
-}
+  SECTION("test_-=operator")
+  {
+    Vec2 vecT{3.0f,2.0f};
+    Vec2 vec{5.0f,3.0f};
+    vec-=vecT;
+    REQUIRE(vec.x==2.0f);
+    REQUIRE(vec.y==1.0f);
+  }
 
-TEST_CASE ("test_vec2_-=operator","[vec2_operator]")
-{
-  Vec2 vecT{3.0f,2.0f};
-  Vec2 vec{5.0f,3.0f};
-  vec-=vecT;
-  REQUIRE(vec.x==2.0f);
-  REQUIRE(vec.y==1.0f);
-}
+  SECTION("test_*=operator")
+  { 
+    Vec2 vec{3.0f,2.0f};
+    float s = 2.0f;
+    vec*=s;
+    REQUIRE(vec.x==6.0f);
+    REQUIRE(vec.y==4.0f);
+  }
 
-TEST_CASE ("test_vec2_*=operator","[vec2_operator]")
-{
-  Vec2 vec{3.0f,2.0f};
-  float s = 2.0f;
-  vec*=s;
-  REQUIRE(vec.x==6.0f);
-  REQUIRE(vec.y==4.0f);
-}
-
-TEST_CASE ("test_vec2_/=operator","[vec2_operator]")
-{
-  Vec2 vec{3.0f,2.0f};
-  float s = 2.0f;
-  vec/=s;
-  REQUIRE(vec.x==1.5f);
-  REQUIRE(vec.y==1.0f);
+  SECTION("test_/=operator")
+  {
+    Vec2 vec{3.0f,2.0f};
+    float s = 2.0f;
+    vec/=s;
+    REQUIRE(vec.x==1.5f);
+    REQUIRE(vec.y==1.0f);
   
-  float z = 0.0f;
-  Vec2 vec_2{3.0f,2.0f};
-  vec_2/=z;
-  REQUIRE(vec_2.x==3.0f);
-  REQUIRE(vec_2.y==2.0f);
+    float z = 0.0f;
+    Vec2 vec_2{3.0f,2.0f};
+    vec_2/=z;
+    REQUIRE(vec_2.x==3.0f);
+    REQUIRE(vec_2.y==2.0f);
+  }
+
+  //aufgabe 2.4
+  SECTION("test_+operator")
+  {
+    Vec2 vec{3.0f,2.0f};
+    Vec2 vecT{2.0f,5.0f};
+    Vec2 sum = vec+vecT;
+    REQUIRE(sum.x==5.0f);
+    REQUIRE(sum.y==7.0f);
+  }
+
+  SECTION("test_-operator")
+  {
+    Vec2 vec{3.0f,2.0f};
+    Vec2 vecT{2.0f,5.0f};
+    Vec2 sub = vec-vecT;
+    REQUIRE(sub.x==1.0f);
+    REQUIRE(sub.y==-3.0f);
+  }
+
+  SECTION("test_*operator")
+  {
+    Vec2 vec{3.0f,2.0f};
+    float s = 2.0f;
+    Vec2 prod =vec*s;
+    REQUIRE(prod.x==6.0f);
+  }
+
+  SECTION("test_/operator")
+  {
+    Vec2 vec{3.0f,2.0f};
+    float s = 2.0f;
+    Vec2 div = vec/s;
+    REQUIRE(div.x == 1.5f);
+    REQUIRE(div.y == 1.0f);
+
+    Vec2 vecT{3.0f,2.0f};
+    float n = 0.0f;
+    Vec2 divi = vecT/n;
+    REQUIRE(divi.x == 3.0f);
+    REQUIRE(divi.y == 2.0f);
+  }
 }
 
-//aufgabe 2.4
-
-TEST_CASE ("test_vec2_+operator","[vec2_operator]")
+TEST_CASE ("test_mat2_operators","[mat2]")
 {
-  Vec2 vec{3.0f,2.0f};
-  Vec2 vecT{2.0f,5.0f};
-  Vec2 sum = vec+vecT;
-  REQUIRE(sum.x==5.0f);
-  REQUIRE(sum.y==7.0f);
+  //aufgabe 2.5
+  SECTION("test_*=operator")
+  {
+    Vec2 v1{3.0f,2.0f};
+    Vec2 v2{2.0f,5.0f};
+    Vec2 v3{2.0f,4.0f};
+    Vec2 v4{3.0f,-2.0f};
+
+    Mat2 m1{v1,v2};
+    Mat2 m2{v3,v4};
+    m1 *= m2;
+    REQUIRE(m1.s1.x == 14.0f);
+    REQUIRE(m1.s1.y == 24.0f);
+    REQUIRE(m1.s2.x == 5.0f);
+    REQUIRE(m1.s2.y == -4.0f);
+  }
+
+  SECTION ("test_*operator")
+  {
+    Vec2 v_1{3.0f,2.0f};
+    Vec2 v_2{2.0f,5.0f};
+    Vec2 v_3{2.0f,4.0f};
+    Vec2 v_4{3.0f,-2.0f};
+
+    Mat2 m1{v_1,v_2};
+    Mat2 m2{v_3,v_4};
+
+    Mat2 erg;
+    erg = m1 * m2;
+    REQUIRE(erg.s1.x == 14.0f);
+    REQUIRE(erg.s1.y == 24.0f);
+    REQUIRE(erg.s2.x == 5.0f);
+    REQUIRE(erg.s2.y == -4.0f);
+  }
+
+  //aufgabe 2.6
+  SECTION ("t2_*vecOperator")
+  {
+    Vec2 v_1{3.0f,2.0f};
+    Vec2 v_2{2.0f,5.0f};
+    Vec2 v_3{3.0f,-2.0f};
+
+    Mat2 m1{v_1,v_2};
+
+    Vec2 erg;
+    erg = m1 * v_3;
+    REQUIRE(erg.x == 5.0f);
+    REQUIRE(erg.y == -4.0f);
+
+    erg = v_3 * m1;
+    REQUIRE(erg.x == 5.0f);
+    REQUIRE(erg.y == -4.0f);
+  }
+
+  SECTION("test_*float_operator")
+  {
+    Vec2 v_1{3.0f,2.0f};
+    Vec2 v_2{2.0f,5.0f};
+
+    Mat2 m1{v_1,v_2};
+
+    Mat2 erg = m1*2;
+    REQUIRE(erg.s1.x == 6.0f);
+    REQUIRE(erg.s1.y == 4.0f);
+    REQUIRE(erg.s2.x == 4.0f);
+    REQUIRE(erg.s2.y == 10.0f);
+
+    erg = 2 * m1;
+    REQUIRE(erg.s1.x == 6.0f);
+    REQUIRE(erg.s1.y == 4.0f);
+    REQUIRE(erg.s2.x == 4.0f);
+    REQUIRE(erg.s2.y == 10.0f);
+  }
 }
-
-TEST_CASE ("test_vec2_-operator","[vec2_operator]")
+TEST_CASE ("test_mat2_functions","[mat2]")
 {
-  Vec2 vec{3.0f,2.0f};
-  Vec2 vecT{2.0f,5.0f};
-  Vec2 sub = vec-vecT;
-  REQUIRE(sub.x==1.0f);
-  REQUIRE(sub.y==-3.0f);
-}
+  SECTION("test_determinante") 
+  {
+    Vec2 v_1{3.0f,-2.0f};
+    Vec2 v_2{2.0f,-5.0f};
 
-TEST_CASE ("test_vec2_*operator","[vec2_operator]")
-{
-  Vec2 vec{3.0f,2.0f};
-  float s = 2.0f;
-  Vec2 prod =vec*s;
-  REQUIRE(prod.x==6.0f);
-}
+    Mat2 m1{v_1,v_2};
 
-TEST_CASE ("test_vec2_/operator","[vec2_operator]")
-{
-  Vec2 vec{3.0f,2.0f};
-  float s = 2.0f;
-  Vec2 div = vec/s;
-  REQUIRE(div.x == 1.5f);
-  REQUIRE(div.y == 1.0f);
+    REQUIRE(m1.det() == -11.0f);
+  }
 
-  Vec2 vecT{3.0f,2.0f};
-  float n = 0.0f;
-  Vec2 divi = vecT/n;
-  REQUIRE(divi.x == 3.0f);
-  REQUIRE(divi.y == 2.0f);
-}
+  SECTION("test_inverse")
+  {
+    Vec2 v_1{3.0f,-2.0f};
+    Vec2 v_2{2.0f,-5.0f};
+    Vec2 v_3{0.45454547f,-0.181818187f};
+    Vec2 v_4{0.181818187f,-0.272727281f};
 
-//aufgabe 2.5
-TEST_CASE ("test_mat2_*=operator","[mat2_operator]")
-{
-  Vec2 v1{3.0f,2.0f};
-  Vec2 v2{2.0f,5.0f};
-  Vec2 v3{2.0f,4.0f};
-  Vec2 v4{3.0f,-2.0f};
+    Mat2 m1{v_1,v_2};
 
-  Mat2 m1{v1,v2};
-  Mat2 m2{v3,v4};
-  m1 *= m2;
-  REQUIRE(m1.s1.x == 14.0f);
-  REQUIRE(m1.s1.y == 24.0f);
-  REQUIRE(m1.s2.x == 5.0f);
-  REQUIRE(m1.s2.y == -4.0f);
-}
+    Mat2 erg{v_3,v_4};
+    Mat2 calc = inverse(m1);
+    REQUIRE(erg.s1.x==Approx(calc.s1.x));
+    REQUIRE(erg.s1.y==Approx(calc.s1.y));
+    REQUIRE(erg.s2.x==Approx(calc.s2.x));
+    REQUIRE(erg.s2.y==Approx(calc.s2.y));
 
-TEST_CASE ("test_mat2_*=float_operator","[mat2_operator]")
-{
+    Vec2 v_5{2.0f,2.0f};
+    Vec2 v_6{2.0f,2.0f};
 
-}
+    Mat2 m2{v_5,v_6};
+    calc = inverse(m2);
+    REQUIRE(calc.s1.x == m2.s1.x);
+    REQUIRE(calc.s1.y == m2.s1.y);
+    REQUIRE(calc.s2.x == m2.s2.x);
+    REQUIRE(calc.s2.y == m2.s2.y);
+  }
 
-TEST_CASE ("test_mat2_*operator","[mat2_operator]")
-{
-  Vec2 v_1{3.0f,2.0f};
-  Vec2 v_2{2.0f,5.0f};
-  Vec2 v_3{2.0f,4.0f};
-  Vec2 v_4{3.0f,-2.0f};
+  SECTION("test_transpose")
+  {
+    Vec2 v_1{3.0f,-2.0f};
+    Vec2 v_2{2.0f,-5.0f};
+    Mat2 m1{v_1,v_2};
 
-  Mat2 m1{v_1,v_2};
-  Mat2 m2{v_3,v_4};
+    Mat2 erg{Vec2{3.0f,2.0f},Vec2{-2.0f,-5.0f}};
+    Mat2 calc = transpose(m1);
+    REQUIRE(calc.s1.x == erg.s1.x);
+    REQUIRE(calc.s2.x == erg.s2.x);
+    REQUIRE(calc.s1.y == erg.s1.y);
+    REQUIRE(calc.s2.y == erg.s2.y);
+  }
 
-  Mat2 erg;
-  erg = m1 * m2;
-  REQUIRE(erg.s1.x == 14.0f);
-  REQUIRE(erg.s1.y == 24.0f);
-  REQUIRE(erg.s2.x == 5.0f);
-  REQUIRE(erg.s2.y == -4.0f);
-}
-
-//aufgabe 2.6
-TEST_CASE ("test_mat2_*vec_operator","[mat2_operator]")
-{
-  Vec2 v_1{3.0f,2.0f};
-  Vec2 v_2{2.0f,5.0f};
-  Vec2 v_3{3.0f,-2.0f};
-
-  Mat2 m1{v_1,v_2};
-
-  Vec2 erg;
-  erg = m1 * v_3;
-  REQUIRE(erg.x == 5.0f);
-  REQUIRE(erg.y == -4.0f);
-
-  erg = v_3 * m1;
-  REQUIRE(erg.x == 5.0f);
-  REQUIRE(erg.y == -4.0f);
-}
-
-TEST_CASE ("test_mat2_*float_operator")
-{
-  Vec2 v_1{3.0f,2.0f};
-  Vec2 v_2{2.0f,5.0f};
-
-  Mat2 m1{v_1,v_2};
-
-  Mat2 erg = m1*2;
-  REQUIRE(erg.s1.x == 6.0f);
-  REQUIRE(erg.s1.y == 4.0f);
-  REQUIRE(erg.s2.x == 4.0f);
-  REQUIRE(erg.s2.y == 10.0f);
-
-  erg = 2 * m1;
-  REQUIRE(erg.s1.x == 6.0f);
-  REQUIRE(erg.s1.y == 4.0f);
-  REQUIRE(erg.s2.x == 4.0f);
-  REQUIRE(erg.s2.y == 10.0f);
-}
-
-TEST_CASE ("test_mat2_determinante","[mat2_det]")
-{
-  Vec2 v_1{3.0f,-2.0f};
-  Vec2 v_2{2.0f,-5.0f};
-
-  Mat2 m1{v_1,v_2};
-
-  REQUIRE(m1.det() == -11.0f);
-}
-
-TEST_CASE ("test_mat2_inverse","[mat2_inv]")
-{
-  Vec2 v_1{3.0f,-2.0f};
-  Vec2 v_2{2.0f,-5.0f};
-  Vec2 v_3{0.45454547f,-0.181818187f};
-  Vec2 v_4{0.181818187f,-0.272727281f};
-
-  Mat2 m1{v_1,v_2};
-
-  Mat2 erg{v_3,v_4};
-  Mat2 calc = inverse(m1);
-  REQUIRE(erg.s1.x==Approx(calc.s1.x));
-  REQUIRE(erg.s1.y==Approx(calc.s1.y));
-  REQUIRE(erg.s2.x==Approx(calc.s2.x));
-  REQUIRE(erg.s2.y==Approx(calc.s2.y));
-
-  Vec2 v_5{2.0f,2.0f};
-  Vec2 v_6{2.0f,2.0f};
-
-  Mat2 m2{v_5,v_6};
-  calc = inverse(m2);
-  REQUIRE(calc.s1.x == m2.s1.x);
-  REQUIRE(calc.s1.y == m2.s1.y);
-  REQUIRE(calc.s2.x == m2.s2.x);
-  REQUIRE(calc.s2.y == m2.s2.y);
-}
-
-TEST_CASE ("test_mat2_transpose","[mat2_trans]")
-{
-  Vec2 v_1{3.0f,-2.0f};
-  Vec2 v_2{2.0f,-5.0f};
-  Mat2 m1{v_1,v_2};
-
-  Mat2 erg{Vec2{3.0f,2.0f},Vec2{-2.0f,-5.0f}};
-  Mat2 calc = transpose(m1);
-  REQUIRE(calc.s1.x == erg.s1.x);
-  REQUIRE(calc.s2.x == erg.s2.x);
-  REQUIRE(calc.s1.y == erg.s1.y);
-  REQUIRE(calc.s2.y == erg.s2.y);
-}
-
-TEST_CASE ("test_mat2_makeRotate","[mat2_rot]")
-{
-  Mat2 calc = make_rotate_mat2(30.0f);
-  REQUIRE(calc.s1.x == Approx(cos(30.0f)));
-  REQUIRE(calc.s1.y == Approx(sin(30.0f)));
-  REQUIRE(calc.s2.x == Approx(-sin(30.0f)));
-  REQUIRE(calc.s2.y == Approx(cos(30.0f)));
+  SECTION("test_mat2_makeRotate")
+  {
+    Mat2 calc = make_rotate_mat2(30.0f);
+    REQUIRE(calc.s1.x == Approx(cos(30.0f)));
+    REQUIRE(calc.s1.y == Approx(sin(30.0f)));
+    REQUIRE(calc.s2.x == Approx(-sin(30.0f)));
+    REQUIRE(calc.s2.y == Approx(cos(30.0f)));
+  }
 }
 
